d3e40f603576900e2b4869ad45f896722747e5a9
IgorAstafev <programmer77@yandex.ru>
2023-02-02 03:15:11 +0300

    feat: create a test for the validity of the release date

--- src/main/java/ru/yandex/practicum/filmorate/validator/DateAfterValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/DateAfterValidator.java
@@ -7,18 +7,19 @@ import java.time.format.DateTimeFormatter;
 
 public class DateAfterValidator implements ConstraintValidator<After, LocalDate> {
 
-    private String currentDate;
+    private String current;
     private String pattern;
 
     @Override
     public void initialize(After constraintAnnotation) {
-        currentDate = constraintAnnotation.currentDate();
+        current = constraintAnnotation.currentDate();
         pattern = constraintAnnotation.pattern();
     }
 
     @Override
     public boolean isValid(LocalDate date, ConstraintValidatorContext constraintValidatorContext) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
-        return date.isAfter(LocalDate.parse(currentDate, formatter));
+        LocalDate currentDate = LocalDate.parse(current, formatter);
+        return date.isAfter(currentDate) || date.isEqual(currentDate);
     }
 }
---
+++ src/test/java/ru/yandex/practicum/filmorate/validator/DateAfterValidatorTest.java
@@ -0,0 +1,56 @@
+package ru.yandex.practicum.filmorate.validator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DateAfterValidatorTest {
+
+    private Validator validator;
+
+    @BeforeEach
+    void setupValidatorInstance() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    @Test
+    void isValid_shouldCheckTheReleaseDateIsAfter28121895() {
+        Film film = new Film(1, "Film Updated", "New film update decriptio",
+                LocalDate.of(1989, 4, 17), 190);
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    void isValid_shouldCheckTheReleaseDateIsBefore28121895() {
+        Film film = new Film(1, "Film Updated", "New film update decriptio",
+                LocalDate.of(1800, 1, 9), 190);
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+
+        assertFalse(violations.isEmpty());
+
+        String message = "Release date must be no earlier than 28.12.1895";
+        assertTrue(violations.stream()
+                .map(ConstraintViolation::getMessage)
+                .anyMatch(message::equals));
+    }
+
+    @Test
+    void isValid_shouldCheckTheReleaseDateIsEqual28121895() {
+        Film film = new Film(1, "Film Updated", "New film update decriptio",
+                LocalDate.of(1895, 12, 28), 190);
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+
+        assertTrue(violations.isEmpty());
+    }
+}
