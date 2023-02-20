b74261b4f91d67f8fcc9a96eae328fb6f2f6e916
IgorAstafev <programmer77@yandex.ru>
2023-02-20 04:42:04 +0300

    fix: fix ErrorHandler

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -38,9 +38,9 @@ public class ErrorHandler {
         return new ErrorResponse(e.getMessage());
     }
 
-/*    @ExceptionHandler
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleThrowable(final Throwable e) {
         return new ErrorResponse("An unexpected error has occurred");
-    }*/
+    }
 }
