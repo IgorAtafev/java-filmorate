57d8ee4ce0b3c31d4b4f849bf03f2a4ac73e67a2
IgorAstafev <programmer77@yandex.ru>
2023-03-30 13:42:10 +0300

    feat: fix bugs in tests

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
@@ -17,6 +17,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
