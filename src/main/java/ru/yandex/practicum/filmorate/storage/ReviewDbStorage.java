327dd36b6858a2903692b393c31550740f720e88
olga <OlOrlovaS@yandex.ru>
2023-03-31 15:13:21 +0300

    style: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/ReviewDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/ReviewDbStorage.java
@@ -12,7 +12,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
